d9554369298a9b4911043aa00d856a84515d9923
SquareDaniel <115992711+SquareDaniel@users.noreply.github.com>
2025-06-09 16:12:01 +0300

    Fix configuration and authentication

--- models.py
+++ models.py
@@ -1,13 +1,22 @@
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from extensions import db
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     is_teacher = db.Column(db.Boolean, default=False)
     courses = db.relationship('Course', backref='author', lazy=True)
+
+    def set_password(self, password: str) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
+
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +24,7 @@ class Course(db.Model):
     description = db.Column(db.Text, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     lessons = db.relationship('Lesson', backref='course', lazy=True)
+
 
 class Lesson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
